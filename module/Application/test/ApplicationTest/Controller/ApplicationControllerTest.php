f24215bb027925e1e5e5e6d17ccc989c224f0562
Giancarlo Mariot <gm@rockstardev.co>
2015-10-02 11:56:05 +0100

    Added quick tests for the index controller

--- module/Application/test/ApplicationTest/Controller/ApplicationControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/ApplicationControllerTest.php
@@ -1,2 +1,27 @@
 <?php
-// Stuff goes here...
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class ApplicationControllerTest extends AbstractHttpControllerTestCase {
+    
+    public function setUp() {
+        // getcwd() reports the tests execute from the BBCTest directory:
+        $configFile = realpath('config/application.config.php');
+        $this->setApplicationConfig(
+            include $configFile
+        );
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed() {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('home');
+    }
+}
