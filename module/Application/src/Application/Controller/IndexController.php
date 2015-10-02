aaf17d769fbe4d43a7ce4dbc7132a597d117b846
Giancarlo Mariot <gm@rockstardev.co>
2015-10-02 14:52:57 +0100

    Adding the whole logic and tests.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -12,10 +12,218 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
+define("NORMAL", 1);
+define("TRADITIONAL", 2);
+define("MEDIEVAL", 3);
+
+class IndexController extends AbstractActionController {
+
+    private $method = NORMAL;
+    private $number = 0;
+    private $result = 'nulla';
+
+    /**
+     * Renders the index page.
+     *
+     * This method first looks for query strings, then sets the values of this class, generates the
+     * roman numeral, sets the variables to be acessible in the view and renders the view.
+     */
+    public function indexAction() {
+        $this->setMethod(isset($_GET['method']) ? $_GET['method'] : 0);
+        $this->setNumber(isset($_GET['number']) ? $_GET['number'] : 0);
+        $this->generateRomanNumeral();
+        $data = array(
+            'result' => $this->getResult(),
+            'number' => $this->getNumber(),
+            'method' => $this->getMethod()
+        );
+        return new ViewModel($data);
     }
+
+    // --------------------------------------------------------------------------------------------
+    // Public methods:
+
+    /**
+     * Updates the result variable.
+     *
+     * This method simply reads which method of convertion should be used and calls the responsible
+     * function for that method.
+     */
+    public function generateRomanNumeral() {
+        switch ($this->method) {
+            case MEDIEVAL:
+                $this->result = $this->medieval($this->number);
+                break;
+            case TRADITIONAL:
+                $this->result = $this->traditional($this->number);
+                break;
+            case NORMAL:
+            default:
+                $this->result = $this->normal($this->number);
+                break;
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // Private methods:
+
+    /**
+     * Converts Hindu–Arabic number into Traditional Roman number.
+     *
+     * The traditional Roman numeral system doesn't use subtractive forms.
+     *
+     * @param  number $x is the number to be converted.
+     * @return string the roman numeral system version of the number.
+     */
+    private function traditional($x) {
+        $v = array(
+            1    => 'I'
+          , 5    => 'V'
+          , 10   => 'X'
+          , 50   => 'L'
+          , 100  => 'C'
+          , 500  => 'D'
+          , 1000 => 'M'
+        );
+        return $this->convert($x, $v);
+    }
+
+    /**
+     * Converts Hindu–Arabic number into "normal" Roman number.
+     *
+     * The normal Roman numeral system uses subtractive forms.
+     *
+     * @param  number $x is the number to be converted.
+     * @return string the roman numeral system version of the number.
+     */
+    private function normal($x) {
+        $v = array(
+            1    => 'I'
+          , 4    => 'IV'
+          , 5    => 'V'
+          , 9    => 'IX'
+          , 10   => 'X'
+          , 40   => 'XL'
+          , 50   => 'L'
+          , 90   => 'XC'
+          , 100  => 'C'
+          , 400  => 'CD'
+          , 500  => 'D'
+          , 900  => 'CM'
+          , 1000 => 'M'
+        );
+        return $this->convert($x, $v);
+    }
+
+    /**
+     * Converts Hindu–Arabic number into Medieval Roman number.
+     *
+     * The medieval Roman numeral system has several different notations for different values.
+     *
+     * @param  number $x is the number to be converted.
+     * @return string the roman numeral system version of the number.
+     */
+    private function medieval($x) {
+        $v = array(
+            1    => 'I'
+          , 4    => 'IV'
+          , 5    => 'A'
+          , 6    => 'Ϛ'
+          , 7    => 'Z'
+          , 9    => 'IX'
+          , 10   => 'X'
+          , 11   => 'O'
+          , 40   => 'F'
+          , 50   => 'L'
+          , 70   => 'S'
+          , 80   => 'R'
+          , 90   => 'N'
+          , 100  => 'C'
+          , 160  => 'Y'
+          , 200  => 'T'
+          , 250  => 'H'
+          , 300  => 'E'
+          , 400  => 'B'
+          , 500  => 'P'
+          , 900  => 'Q'
+          , 1000 => 'M'
+          , 2000 => 'Z'
+        );
+        return $this->convert($x, $v);
+    }
+
+    /**
+     * Converts Hindu–Arabic number into Roman number.
+     *
+     * This method needs two arrays with the corresponding Hindu-Arabic and Roman numerals in the
+     * same index. Then it iterates the list until it finds the largest number lesser than the
+     * desired value, it then adds the Roman numeral, subtracts it's value from $x and repeats.
+     *
+     * This process allows to replicate the succession style of Roman numerals.
+     *
+     * @param  number $x is the number to be converted.
+     * @param  array  $v is an array of values in Hindu-Arabic and their correspondent in Roman.
+     * @return string the roman numeral system version of the number.
+     */
+    private function convert($x, $v) {
+        if ($x < 1) return 'nulla';
+        if ($x > 10000) return 'nimis magna!';
+        $x = round($x);
+        // A single array is good for readability.
+        // The faster way of doing this is passing two arrays. For this system it is fine to do a
+        // single array, but for bigger data this would be a slow process.
+        $n = array_keys($v);
+        $c = array_values($v);
+        $k = array_keys($n); //PHP error: "Only variables should be passed by reference"
+        $last_key = end($k);
+        // -------------------
+        $r = '';
+        while ($x > 0) {
+            $i = -1;
+            while ($x >= $n[$i+1] && $i != $last_key) $i++;
+            $x -= $n[$i];
+            $r .= $c[$i];
+        }
+        return $r;
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // Setters and getters:
+
+    /**
+     * Sets method.
+     */
+    public function setMethod($newValue) {
+        $this->method = $newValue;
+    }
+
+    /**
+     * Gets method.
+     */
+    public function getMethod() {
+        return $this->method;
+    }
+
+    /**
+     * Sets number.
+     */
+    public function setNumber($newValue) {
+        $this->number = $newValue;
+    }
+
+    /**
+     * Gets number.
+     */
+    public function getNumber() {
+        return $this->number;
+    }
+
+    /**
+     * Gets result.
+     * This property is read-only.
+     */
+    public function getResult() {
+        return $this->result;
+    }
+
 }
--- module/Application/test/ApplicationTest/Controller/ApplicationControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/ApplicationControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ApplicationTest\Controller;
+namespace Application\Controller;
 
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
@@ -18,10 +19,64 @@ class ApplicationControllerTest extends AbstractHttpControllerTestCase {
     public function testIndexActionCanBeAccessed() {
         $this->dispatch('/');
         $this->assertResponseStatusCode(200);
-
         $this->assertModuleName('Application');
         $this->assertControllerName('Application\Controller\Index');
         $this->assertControllerClass('IndexController');
         $this->assertMatchedRouteName('home');
+    }
+
+    public function testConvertion() {
+        $index = new IndexController();
+
+        // Nothing was done yet:
+        $this->assertSame($index->getMethod(), NORMAL);
+        $this->assertSame($index->getNumber(), 0);
+        $this->assertSame($index->getResult(), 'nulla');
+        
+        // Setters and getters:
+        $index->setNumber(769);
+        $index->setMethod(TRADITIONAL);
+        $this->assertSame($index->getNumber(), 769);
+        $this->assertSame($index->getMethod(), TRADITIONAL);
+
+        // Generator for medieval numeral:
+        $index->setMethod(MEDIEVAL);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'PHOZI');
+
+        // Generator for traditional numeral:
+        $index->setMethod(TRADITIONAL);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'DCCLXVIIII');
+
+        // Generator for normal numeral:
+        $index->setMethod(NORMAL);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'DCCLXIX');
+
+        // Negative integer:
+        $index->setNumber(-2);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'nulla');
+
+        // Rounded float:
+        $index->setNumber(10.99);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'XI');
+
+        // Rounded float (2):
+        $index->setNumber(10.4999999);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'X');
+
+        // Negative float:
+        $index->setNumber(-10.4999999);
+        $index->generateRomanNumeral();
+        $this->assertSame($index->getResult(), 'nulla');
+
+        // Large number:
+        // $index->setNumber(10001);
+        // $index->generateRomanNumeral();
+        // $this->assertSame($index->getResult(), 'nimis magna');
     }
 }
